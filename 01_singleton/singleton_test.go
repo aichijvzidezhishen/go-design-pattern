c20b931c486f9540c327fdc6cef226f682ae4a89
“zhishen” <1306068060@qq.com>
2022-10-29 10:41:12 +0800

    test

--- 01_singleton/singleton_test.go
+++ 01_singleton/singleton_test.go
@@ -12,6 +12,11 @@ func TestGetInstance(t *testing.T) {
 	assert.Equal(t, singleton.GetInstance(), singleton.GetInstance())
 }
 
+// The body function will be run in each goroutine. It should set up any
+// goroutine-local state and then iterate until pb.Next returns false.
+// It should not use the StartTimer, StopTimer, or ResetTimer functions,
+// because they have global effect. It should also not call Run.
+
 func BenchmarkGetInstanceParallel(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
